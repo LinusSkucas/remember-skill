1fc097b2e021d0419c286cc6ba541598172a04c0
Linus S <LinusS1@users.norelply.github.com>
2018-08-07 15:36:51 -0700

    Fixed intent management

--- __init__.py
+++ __init__.py
@@ -1,20 +1,24 @@
 from mycroft import MycroftSkill, intent_handler
 from adapt.intent import IntentBuilder
+from mycroft.util.log import getLogger
+
+__author__ = "LinusS1"
+
+LOGGER = getLogger(__name__)
 
 class RememberSkill(MycroftSkill):
     def __init__(self):
         super(RememberSkill, self).__init__(name='RememberSkill')
 
-    @intent_handler(IntentBuilder("RememberIntent").require("remember").require("thought"))
+    @intent_handler(IntentBuilder("RememberIntent").require("remember").optionally("thought"))
     def handle_remember(self, message):
         thought = message.data.get("thought")
-        self.settings["thought"] = str(thought)
-        self.speak_dialog("remember")
-
-    @intent_handler(IntentBuilder("RememberIntent").require("recall"))
-    def handle_recall(self, message):
-        thought = self.settings.get("thought")
-        self.speak_dialog("recall", data={"thought" : str(thought)})
+        if "what" in message.data["utterance"]:
+            thought = self.settings.get("thought")
+            self.speak_dialog("recall", data={"thought" : thought})
+        else:
+            self.settings["thought"] = str(thought)
+            self.speak_dialog("remember")
 
 def create_skill():
     return RememberSkill()
