ac2eb905c6090b4d999c55e70e7cab0096ea621f
Linus S <LinusS1@users.norelply.github.com>
2018-08-07 12:05:41 -0700

    fixed bugs

--- __init__.py
+++ __init__.py
@@ -3,15 +3,15 @@ from adapt.intent import IntentBuilder
 
 class RememberSkill(MycroftSkill):
     def __init__(self):
-        super(RememberSkill, self).__init__(name'RememberSkill')
+        super(RememberSkill, self).__init__(name='RememberSkill')
 
-    @intent_handler(IntentBuilder("RememberIntent").require("remember").require("thought")
+    @intent_handler(IntentBuilder("RememberIntent").require("remember").require("thought"))
     def handle_remember(self, message):
         thought = message.data.get("thought")
         self.settings["thought"] = str(thought)
         self.speak_dialog("remember")
 
-    @intent_handler(IntentBuilder("RememberIntent").require("recall").require("remember"))
+    @intent_handler(IntentBuilder("RememberIntent").require("recall"))
     def handle_recall(self, message):
         thought = self.settings.get("thought")
         self.speak_dialog("recall", data={"thought" : str(thought)})
